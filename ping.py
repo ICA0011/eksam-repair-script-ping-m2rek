4a56fcf4dd05839349af0375943dee94273f1e6f
Marek <marekott@hot.ee>
2022-05-26 12:14:43 +0300

    commit_1

--- ping.py
+++ ping.py
@@ -1,8 +1,16 @@
+from os import strerror
 import requests
 
-def check_server_status():
-    a = requests.get(url)
-    if a = 200:
-        return True
-    else:
-        return False
+def check_server_status(url: str):
+    while True:
+        try:
+            a = requests.get(url)
+            if a.status_code == 200:
+                print('OK')
+                return True
+
+        except Exception:
+            print('NOK')
+            return False
+
+check_server_status('http://itcollege.ee')
